12602e7537238f7d9581369eee6902e398f9b456
irfanguvian <guvian14@gmail.com>
2021-07-11 18:50:22 +0700

    finished elements/form/InputDate with react-date-range and utils/formatDate

---
+++ src/elements/Form/InputDate/index.js
@@ -0,0 +1,92 @@
+import React, { useState, useRef, useEffect } from 'react'
+import propTypes from 'prop-types'
+
+import { DateRange } from 'react-date-range'
+
+import './index.scss'
+import 'react-date-range/dist/styles.css' // main style file
+import 'react-date-range/dist/theme/default.css' // theme css file
+
+import IconCalender from 'assets/images/icons/icon-calendar.svg'
+import { formatDate } from 'tough-cookie'
+
+export default function Date(props) {
+  const { value, placeholder, name } = props
+  const [isShowed, setIsShowed] = useState(false)
+
+  const dataPickerChange = (value) => {
+    const target = {
+      target: {
+        value: value.selection,
+        name: name,
+      },
+    }
+    props.onchange(target)
+  }
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside)
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  })
+
+  const refDate = useRef(null)
+
+  const handleClickOutside = (event) => {
+    if (refDate && !refDate.current.contains(event.target)) {
+      setIsShowed(false)
+    }
+  }
+
+  const check = (focus) => {
+    focus.indexOf(1) < 0 && setIsShowed(false)
+  }
+
+  const displayDate = `${value.startDate ? formatDate(value.startDate) : ''}${
+    value.endDate ? ' - ' + formatDate(value.endDate) : ''
+  }`
+
+  return (
+    <div
+      ref={refDate}
+      className={['input-date mb-3', props.outerClassName].join(' ')}
+    >
+      <div className='input-group'>
+        <div className='input-group-prepend bg-gray-900'>
+          <span className='input-group-text'>
+            <img src={IconCalender} alt='iconCalender' />
+          </span>
+        </div>
+        <input
+          readOnly
+          type='text'
+          className='form-control'
+          value={displayDate}
+          placeholder={placeholder}
+          onClick={() => setIsShowed(!isShowed)}
+        />
+
+        {isShowed && (
+          <div className='date-range-wrapper'>
+            <DateRange
+              editableDateInputs={true}
+              onchange={dataPickerChange}
+              moveRangeOnFirstSelection={false}
+              onRangeFocusChange={check}
+              ranges={[value]}
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+Date.propTypes = {
+  value: propTypes.object,
+  onchange: propTypes.func,
+  placeholder: propTypes.string,
+  outerClassName: propTypes.string,
+}
